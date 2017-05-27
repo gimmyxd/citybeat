73bf093c23adab03a8ee7c3623abcc604e47edfb
Cristi Zanfir <cristian.zanfir@unifiedpost.com>
2017-05-27 09:55:00 +0300

    Create Volunteer model and migration. Added .idea to gitignore

---
+++ db/migrate/20170527064556_create_volunteers.rb
@@ -0,0 +1,15 @@
+class CreateVolunteers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :volunteers do |t|
+      t.string :firstname
+      t.string :lastname
+      t.email :email
+      t.string :phone_number
+      t.integer :age
+      t.string :cnp
+      t.string :gender
+
+      t.timestamps
+    end
+  end
+end
