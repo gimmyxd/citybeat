4f5af076623598ebcd115d38d0ffa7f3cc2f8211
Andra Sintioana <andra.sintioana@unifiedpost.com>
2017-05-27 14:20:25 +0300

    Add Dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,31 +3,31 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+
+    columns do
+      column do
+        panel "Recent Volunteers" do
+          table_for Volunteer.all.limit(5) do
+            column("Firstname")   {|volunteer| volunteer.firstname }
+            column("Lastname"){|volunteer| volunteer.lastname }
+            column("Email")   {|volunteer| volunteer.email }
+            column("See details")   {|volunteer| link_to("See",  "/admin/volunteers/#{volunteer.id}") }
+          end
+        end
       end
     end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+    columns do
+      column do
+        panel "Recent Projects" do
+          table_for Project.all.limit(5) do
+            column("Title")   {|project| project.title }
+            column("Status"){|project| project.status }
+            column("See details")   {|project| link_to("See", "/admin/projects/#{project.id}") }
+          end
+        end
+      end
 
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+    end
+  end
 end
