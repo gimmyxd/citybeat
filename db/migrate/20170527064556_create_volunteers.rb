2d6b8abb79bee3aa70b723777887c7aef6294c47
Cristi Zanfir <cristian.zanfir@unifiedpost.com>
2017-05-27 10:00:22 +0300

    Initial schema and db creation

--- db/migrate/20170527064556_create_volunteers.rb
+++ db/migrate/20170527064556_create_volunteers.rb
@@ -3,7 +3,7 @@ class CreateVolunteers < ActiveRecord::Migration[5.0]
     create_table :volunteers do |t|
       t.string :firstname
       t.string :lastname
-      t.email :email
+      t.string :email
       t.string :phone_number
       t.integer :age
       t.string :cnp
