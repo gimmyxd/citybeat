c2ca1f602ec58440ecc685913a88a9f87d2479db
Ionut F <ionut.fechete@unifiedpost.com>
2017-05-27 17:20:42 +0300

    wip

--- app/services/rss_parser.rb
+++ app/services/rss_parser.rb
@@ -1,29 +1,58 @@
 require 'rss'
 class RssParser
-  attribute :feed
-  attribute :keyword
 
-  def call
-    get_feed
-    search_for_keywords
-    filter_results
-    save_articles
+  attr_accessor :feed, :keywords
+
+  def initialize(params)
+    @feed = params[:feed]
+    @keywords = params[:keywords]
   end
 
-  def get_feed
-
+  def parse
+    initialize_feed
+    process_rss_feed if rss?
+    process_atom_feed if atom?
   end
 
-  def search_for_keywords
+  private
 
+  def initialize_feed
+    @rss = RSS::Parser.parse(feed, false)
   end
 
-  def filter_results
+  def process_rss_feed
+    @rss.items.each do |article|
+      if has_title_keyword?(article.title) && has_content_keyword?(article.content_description)
+        save_article({
+            title: article.title,
+            description: article.description,
+            keywords: @matched_keywords,
+            status
+         "organizer_id"
+        "volunteer_id"
+           "photo"
 
+                     })
+      end
+    end
   end
 
-  def save_articles
+  def process_atom_feed
+    @rss.items.each do |article|
 
+    end
+  end
+
+  def save_article(properties)
+    Article.create(properties)
+  end
+
+  def rss?
+    @rss.feed_type == 'rss'
+  end
+
+  def atom?
+    @rss.feed_type == 'atom'
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170527092246) do
+ActiveRecord::Schema.define(version: 20170527125558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,13 @@ ActiveRecord::Schema.define(version: 20170527092246) do
     t.string   "photo"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "rss_feeds", force: :cascade do |t|
+    t.string   "url"
+    t.datetime "last_parsed"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "volunteers", force: :cascade do |t|
