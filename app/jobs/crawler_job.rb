98dce537462214db49095bb596586bce18a796d3
Cristi Zanfir <cristian.zanfir@unifiedpost.com>
2017-05-27 20:47:42 +0300

    Intermediate commit for crawler

--- app/jobs/crawler_job.rb
+++ app/jobs/crawler_job.rb
@@ -1,9 +1,15 @@
 require 'sidekiq'
 require 'sidekiq-cron'
+require 'open-uri'
 class CrawlerJob
   include Sidekiq::Worker
 
-  def perform
-    # do something
+  def perform(url)
+    # this parses the given URL. returns in doc the full html mark-up of the page.
+    # after parsing we return relevant information
+    doc = Nokogiri::HTML(open(url))
+    byebug
+    puts doc
+
   end
 end
--- app/jobs/test.rb
+++ app/jobs/test.rb
@@ -1,3 +1,5 @@
 require 'sidekiq'
 require 'sidekiq-cron'
-Sidekiq::Cron::Job.create(name: 'Hard worker - every 5min', cron: '*/1 * * * *', class: 'CrawlerJob') # execute at every 5 minutes, ex: 12:05, 12:10, 12:15...etc
+Sidekiq::Cron::Job.create(name: 'My crawler', cron: '*/5 * * * *', class: 'CrawlerJob', args: 'http://www.opiniatimisoarei.ro/search/festival') # execute at every 5 minutes, ex: 12:05, 12:10, 12:15...etc
+
+# 0 0 * * * * cron job for daily at midnight
